1c25c01457e39ec44bfd39f3e5fa77f190e4a6c3
Romaka111 <romanmelkonyan@inbox.ru>
2025-07-15 21:16:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 import nest_asyncio
 
 BOT_TOKEN = "7584731929:AAEV05mjsHb_7wl6eqkDwP1yfQ09o2ciklI"
-BASE_URL = os.getenv("BASE_URL")
-WEBHOOK_SECRET_KEY = os.getenv("WEBHOOK_SECRET_KEY")
+BASE_URL = "https://web-production-6325f.up.railway.app"
+WEBHOOK_SECRET_KEY = "supersecret"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Это NeoGPT, я запущен на Webhook с защитой!")
@@ -15,7 +15,7 @@ async def start_bot():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
-    webhook_url = f"https://{BASE_URL}/webhook/{BOT_TOKEN}"
+    webhook_url = f"{BASE_URL}/webhook/{WEBHOOK_SECRET_KEY}"
     await app.bot.set_webhook(url=webhook_url, secret_token=WEBHOOK_SECRET_KEY)
     print("Webhook установлен с секретом:", webhook_url)
 
